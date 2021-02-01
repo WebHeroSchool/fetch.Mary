5cead99575ff8923c0c94eb86be6aaf87753be03
Mariya Shulyak <shulyakmariya@yandex.ru>
2021-02-01 14:29:39 +0300

    Вопрос

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 let name = 'MariyaShulyak';
 let url = 'https://api.github.com/users/';
 
-let date = new Date();
- const getDate = new Promise ((resolve, reject) => {
-   setTimeOut (() => date ? resolve (date) : reject ('err'),3000);
- });
+let currentDate = new Date();
+
+let preloader = document.getElementById ('preloader');
+
+setTimeout(function() {
+	preloader.classList.add('hidden');
+}, 2000);
+
+const getDate = new Promise((resolve, reject) => {
+ setTimeout(() => currentDate ? resolve(currentDate.innerHTML = currentDate.toDateString()) : reject('Не удалось получить дату'), 1500);
+})
+
 
  const getUrl = new Promise ((resolve,reject) => {
-   setTimeOut (() => url ? resolve (url) : reject ('Ссылка не найдена'),2000);
+   setTimeout (() => url ? resolve (url) : reject ('Ссылка не найдена'),1000);
  });
 
  const getName = new Promise ((resolve,reject) => {
-   setTimeOut (() => name ? resolve (name) : reject ('Имя не найдено'),1000);
+   setTimeout (() => name ? resolve (name) : reject ('Имя не найдено'),2000);
  });
 
- Promise.all ([getUrl,getDate,getName])
-   .then (([name,url]) => fetch(`${url}${name}`))
-   .then (response => response.json)
+
+ Promise.all ([getUrl,getName])
+   .then (([url,name]) => fetch(`${url}${name}`))
+   .then (response => response.json())
    .then (json => {
      console.log (json.avatar_url);
      console.log (json.bio);
@@ -27,14 +36,14 @@ let date = new Date();
     img.src = json.avatar_url;
    document.body.append(img);
 
-   let name = document.createElement('p');
+   let name = document.createElement('a');
    if (json.name != null) {
      name.innerHTML = json.name;
    } else {
      name.innerHTML ='Информация о пользователе не доступна';
    };
    document.body.append(name);
-   name.addEventListener("clik", () =>window.location.herf= json.html_url);
+   name.addEventListener("clik", () => window.location.herf = json.html_url);
 
    let bio = document.createElement ('p');
    if (json.bio != null) {
@@ -51,6 +60,6 @@ let date = new Date();
    html_url.innerHTML ='Информация о пользователе не доступна';
    }
    document.body.append(herf);
-   document.body.append (date);
+   document.body.append(currentDate);
  })
  .catch (error => console.error());
